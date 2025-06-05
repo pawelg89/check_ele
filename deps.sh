f3687c384fb7455a2e6f848e4ec3e877d24b7171
Pawel Gardzinski <pawel.gardzinski@gmail.com>
2025-06-05 11:43:31 +0200

    fix deps.sh

--- deps.sh
+++ deps.sh
@@ -2,12 +2,19 @@
 
 pip install -r requirements.txt
 
-# install chrome driver
+# install google chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get install -y chromium-browser chromium-chromedriver
+sudo apt-get install -y google-chrome-stable
 
-# install selenium
+# get chrome version and install matching chromedriver
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d'.' -f1)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -q "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/local/bin/
+rm chromedriver_linux64.zip
 
-# install beautifulsoup4
+# install selenium beautifulsoup4
 pip install selenium beautifulsoup4
-# install requests
