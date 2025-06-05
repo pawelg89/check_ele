45aa1a6097aae192dcec6df91f37f5febfc93b25
Pawel Gardzinski <pawel.gardzinski@gmail.com>
2025-06-05 11:37:59 +0200

    deps.sh

---
+++ deps.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+pip install -r requirements.txt
+
+# install chrome driver
+sudo apt-get update
+sudo apt-get install -y chromium-browser chromium-chromedriver
+
+# install selenium
+
+# install beautifulsoup4
+pip install selenium beautifulsoup4
+# install requests
